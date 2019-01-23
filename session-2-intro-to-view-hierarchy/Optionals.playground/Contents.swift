a1a4c3fcba06acba7468bf3d67a5474e43be31be
Timothy <trediehs@g.ucla.edu>
2019-01-22 21:57:05 -0800

    Changed Playground durring meeting

--- session-2-intro-to-view-hierarchy/Optionals.playground/Contents.swift
+++ session-2-intro-to-view-hierarchy/Optionals.playground/Contents.swift
@@ -1,13 +1,13 @@
 // Optionals
-var maybeInt: Int?
-maybeInt = 5
-maybeInt = nil
+var maybeInt: Int?  //Box -- Might have val
+maybeInt = 5        //Set to int
+maybeInt = nil      //Set to nil
 
 // Why use an Optional?
 class Person {
     
     var name: String
-    var brainPower: Int?
+    var brainPower: Int?    //Not everyone has a brain... like zombies
     
     init(name: String, brainPower: Int?) {
         self.name = name
@@ -21,30 +21,25 @@ class Person {
 }
 
 let kevin = Person(name: "Kevin", brainPower: 10)
-let slug = Person(name: "Sluggie", brainPower: 0)
+print(kevin.brainPower)
+print(kevin.brainPower!)
+let slug = Person(name: "Slug", brainPower: 0)
+print(slug.brainPower)
+print(slug.brainPower!)
 let uscStudent = Person(name: "Trojan USuCk", brainPower: nil)
-
-// Force unwrapping - brute force. Crash the program if there's nothing there.
-func printNames(person: Person) {
-    
-    let name = person.name
-    let brainPower = person.brainPower!
-    
-    print("\(name) has a brain power of \(brainPower).")
-    
-}
-
-printNames(person: kevin)
-// printNames(person: shannon) Crash!
-// Optional chaining - gracefully fail.
-let flakyTeamMember: Person? = nil
-flakyTeamMember?.doWork()
+print(uscStudent.brainPower)
+//print(uscStudent.brainPower!)
 
 // Optional Binding - be safe.
 func safePrintNames(person: Person) {
     
     let name = person.name
     
+    if person.brainPower != nil{
+        let bp = person.brainPower!
+    }
+    
+    //brainPower is local
     if let brainPower = person.brainPower {
         print("\(name) has a brain power of \(brainPower).")
     } else {
@@ -56,3 +51,11 @@ func safePrintNames(person: Person) {
 safePrintNames(person: kevin)
 safePrintNames(person: slug)
 safePrintNames(person: uscStudent)
+
+// Optional chaining - gracefully fail.
+
+//let blakeFromCS35L = Person(name: "Blake", brainPower: -6) //SECOND
+let blakeFromCS35L: Person? = nil   //FIRST
+let flakyTeamMember: Person? = blakeFromCS35L
+flakyTeamMember?.doWork()
+
