d961562b518ec6c6c0ca0d07101939b73e74ab2d
Timothy <trediehs@g.ucla.edu>
2019-01-23 16:38:46 -0800

    Slugs don't have brains.  Changed Slug to Sloth.  Also added some comments.

--- session-2-intro-to-view-hierarchy/Optionals.playground/Contents.swift
+++ session-2-intro-to-view-hierarchy/Optionals.playground/Contents.swift
@@ -1,61 +1,74 @@
 // Optionals
-var maybeInt: Int?  //Box -- Might have val
+var maybeInt: Int?  //Box -- Might have val (this is a variable declaration)
 maybeInt = 5        //Set to int
 maybeInt = nil      //Set to nil
 
 // Why use an Optional?
 class Person {
     
-    var name: String
-    var brainPower: Int?    //Not everyone has a brain... like zombies
+    var name: String        //Everyone has a name
+    var brainPower: Int?    //But not everyone has a brain... like zombies
     
-    init(name: String, brainPower: Int?) {
+    init(name: String, brainPower: Int?) {  //Let's make an initializer
         self.name = name
         self.brainPower = brainPower
     }
     
-    func doWork() {
+    func doWork() {                         //People do work, let's write a function for that.
         print("Work work work work work")
     }
     
 }
 
-let kevin = Person(name: "Kevin", brainPower: 10)
-print(kevin.brainPower)
-print(kevin.brainPower!)
-let slug = Person(name: "Slug", brainPower: 0)
-print(slug.brainPower)
-print(slug.brainPower!)
-let uscStudent = Person(name: "Trojan USuCk", brainPower: nil)
-print(uscStudent.brainPower)
-//print(uscStudent.brainPower!)
+//Now let's make some people
+let kevin = Person(name: "Kevin", brainPower: 10)   //Kevin has a brain, and is pretty smart
+print(kevin.brainPower)                             //1. Print out their brain power
+print(kevin.brainPower!)                            //2. FORCE UNWRAP: Shove your hand in the box!  "It WILL have a value!  It MUST!"
+                                                    //It has to have a value... because I don't know what I'm going to do if it doesn't
 
-// Optional Binding - be safe.
+let sloth = Person(name: "Sloth", brainPower: 0)    //Named after a sloth... sloths are... real stupid
+print(sloth.brainPower)                             //1.
+print(sloth.brainPower!)                            //2.
+
+let uscStudent = Person(name: "Trojan USuCk", brainPower: nil)  //PROBLEM: uscStudents don't have brains!  How do we represent that!?!?!
+print(uscStudent.brainPower)                        //1.
+//print(uscStudent.brainPower!)                     //2. What if the it DOESN'T HAVE A VALUE
+
+// Optional Binding: SAFE.  Let's not crash if it's nil.
 func safePrintNames(person: Person) {
     
     let name = person.name
     
-    if person.brainPower != nil{
+    //Let's test if it's nil
+    if person.brainPower != nil{                        //IF... LET
         let bp = person.brainPower!
+        print("\(name) has a brain power of \(bp).")    //HERE'S A NEW WAY TO PRINT THINGS
+    }                                                   //JUST PUT THE VAR IN THE \() AND IT WILL BE PLACED THERE!
+    else{
+        print("\(name) has no brain.")
     }
     
-    //brainPower is local
-    if let brainPower = person.brainPower {
-        print("\(name) has a brain power of \(brainPower).")
+    //There is a better way: if... let
+    if let bp = person.brainPower {                     //brainPower is local
+        print("\(name) has a brain power of \(bp).")
     } else {
         print("\(name) has no brain.")
     }
     
 }
 
+//Look!  It doesn't crash if we do USC student now!
 safePrintNames(person: kevin)
-safePrintNames(person: slug)
+safePrintNames(person: sloth)
 safePrintNames(person: uscStudent)
 
 // Optional chaining - gracefully fail.
+// You can have an optional for one of your classes
 
-//let blakeFromCS35L = Person(name: "Blake", brainPower: -6) //SECOND
-let blakeFromCS35L: Person? = nil   //FIRST
+//Let's make one for 'Person'
+let blakeFromCS35L: Person? = nil                               //FIRST
+//let blakeFromCS35L = Person(name: "Blake", brainPower: -6)    //SECOND
 let flakyTeamMember: Person? = blakeFromCS35L
-flakyTeamMember?.doWork()
+flakyTeamMember?.doWork()                                       //Oh wow... Blake didn't do work... surprizing
+                                                                //but let's say he did show up ^^^
 
